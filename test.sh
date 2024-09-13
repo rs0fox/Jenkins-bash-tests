e8b864d20b38555cbaff3d98c1811a1d6ac399bd
Rohan S <168646099+rs0fox@users.noreply.github.com>
2024-09-13 09:08:50 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 echo "BUILD_NUMBER" :: $BUILD_NUMBER
 echo "BUILD_ID" :: $BUILD_ID
 echo "BUILD_DISPLAY_NAME" ::$BUILD_DISPLAY_NAME
+echo "Below is the output of entered environments variables in Jenkins"
+printenv
